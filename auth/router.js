45eed6acc14c112119e3c2c8bba4bd8c5d5505a3
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-02-29 02:33:59 -0500

    login working, register err, not returning correct json or 201

--- auth/router.js
+++ auth/router.js
@@ -2,43 +2,43 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const Users = require("./model.js");
+const Auth = require("./model.js");
 const { jwtSecret } = require("../config/secrets.js");
 
 router.post("/register", (req, res) => {
+  // implement registration
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  const hash = bcrypt.hashSync(user.password, 8);
   user.password = hash;
-
-  Users.add(user)
+  const token = generateToken(user);
+  Auth.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      res
+        .status(201)
+        .json({ message: "user created succesfully", user: saved, token });
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 router.post("/login", (req, res) => {
+  // implement login
   let { username, password } = req.body;
-
-  Users.findBy({ username })
+  Auth.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user); // get a token
-
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-          token // send the token
-        });
+        const token = generateToken(user);
+        res.json({ message: "user logged in succesfully", user, token });
       } else {
-        res.status(401).json({ message: "Invalid Credentials" });
+        res.status(401).json({ message: "invalid credentials" });
       }
     })
-    .catch(error => {
-      console.log("ERROR: ", error);
-      res.status(500).json({ error: "/login error" });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
