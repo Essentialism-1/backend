850d5b3e66bd0c74e04b379761e8800a7aaba1e4
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-03-05 12:04:13 -0500

    Update router.js

--- user_values/router.js
+++ user_values/router.js
@@ -43,7 +43,7 @@ router.get("/:id", (req, res) => {
   db("user_values")
     .join("values", "user_values.values_id", "=", "values.id")
     .where("user_values.user_id", req.params.id)
-    .select("user_values.values_id", "values.value", "user_values.description")
+    .select("user_values.user_id", "user_values.values_id", "values.value", "user_values.description")
     .then(vals => res.status(201).json(vals))
     .catch(({ name, message, stack, code }) => {
       console.log({ name, message, stack, code });
